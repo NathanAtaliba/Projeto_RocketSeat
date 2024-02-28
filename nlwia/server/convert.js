031c9f568f86d4c167ee7cff8023a2a7df53f85a
Nathan Santos Ataliba <nathan.s@inatel.br>
2024-02-28 08:37:39 -0300

    final

--- nlwia/server/convert.js
+++ nlwia/server/convert.js
@@ -1,32 +1,33 @@
 import fs from 'fs';
 import wav from 'node-wav';
-import ffmpeg from "fluent-ffmpeg";
+import ffmpeg from 'fluent-ffmpeg';
 import ffmpegStatic from 'ffmpeg-static';
 
-const filePath = "./tmp/audio.mp4";
+const filePath = "../tmp/audio.mp4";
 const outputPath = filePath.replace(".mp4",".wav");
 
 export const convert = () =>
     new Promise((resolve, reject) => {
     console.log("Convertendo o video...");
-
     ffmpeg.setFfmpegPath(ffmpegStatic)
     ffmpeg()
         .input(filePath)
         .audioFrequency(16000)
         .audioChannels(1)
         .format("wav")
-        .on( "end", () => {
+        .on('end', () => {
+            try{
+            console.log("Convertendo o video...");
             const file = fs.readFileSync(outputPath);
             const fileDecoded = wav.decode(file);
-            console.log("Convertendo o video...");
             const audioData = fileDecoded.channelData[0];
             const floatArray = new Float32Array(audioData);
-
             console.log("Video convertido com sucesso!");
-            fs.unlinkSync(outputPath);
             resolve(floatArray);
-        
+            fs.unlinkSync(outputPath);
+            }catch(error){
+                console.log("Erro:", error);
+            }
     })
         .on("error", (error)=>{
             console.log("Erro ao converter o video: ", error);
